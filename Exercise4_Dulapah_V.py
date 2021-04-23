4f4aad4348b43b6544acdcc6fb25b2107f27ca2d
n0miya <dulapah6@gmail.com>
2021-04-23 14:49:56 +0700

    Added Exercise4

---
+++ Exercise4_Dulapah_V.py
@@ -0,0 +1,9 @@
+FoundEng = 50
+GenBuss = 20.5
+IntroComp = 45.2
+CompPro = 40
+
+print("Foundation English : ", FoundEng)
+print("General Business : ", GenBuss)
+print("Introduction to Computer System : ", IntroComp)
+print("Computer Programming : ", CompPro)
